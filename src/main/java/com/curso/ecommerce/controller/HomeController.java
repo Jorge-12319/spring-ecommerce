fb5b9f10451440fee17f8171f8eb0a358fd11d37
unknown <luisjorgejorge191919@hotmail.com>
2022-05-03 22:59:29 -0500

    validacion para doble producto y ver el carrito desde la cabecera del template del usuario menu opcion carrito

--- src/main/java/com/curso/ecommerce/controller/HomeController.java
+++ src/main/java/com/curso/ecommerce/controller/HomeController.java
@@ -75,7 +75,16 @@ public class HomeController {
 		detalleOrden.setTotal(producto.getPrecio()*cantidad);
 		detalleOrden.setProducto(producto);
 		
-		detalles.add(detalleOrden);
+		// validar que el producto no se añada 2 veces
+		Integer idProducto=producto.getId();
+		boolean ingresado = detalles.stream().anyMatch(p -> p.getProducto().getId() == idProducto);
+		
+		
+		if(!ingresado) {
+			detalles.add(detalleOrden);
+		}
+		
+		
 		
 		sumaTotal=detalles.stream().mapToDouble(dt -> dt.getTotal()).sum();
 		
@@ -113,5 +122,15 @@ public class HomeController {
 		
 		return "usuario/carrito";
 	}
+	
+	@GetMapping("/getCart")
+	public String getCart(Model model) {
+		model.addAttribute("cart",detalles);
+		model.addAttribute("orden",orden);
+		
+		
+		return "/usuario/carrito";
+	}
+	
 	
 }
